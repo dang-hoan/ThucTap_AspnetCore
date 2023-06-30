b13313a57fa436b395ce6460131dc1cc5f298527
dang-hoan <nguyenthedanghoan@gmail.com>
2023-06-30 10:19:07 +0700

    override field name to database

---
+++ LearnAspNetCoreMVC/Migrations/20230630031729_ChangeColumnNameOfCategoryTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LearnAspNetCoreMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeColumnNameOfCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Categories",
+                newName: "ProductName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductName",
+                table: "Categories",
+                newName: "Name");
+        }
+    }
+}
--- LearnAspNetCoreMVC/Models/Category.cs
+++ LearnAspNetCoreMVC/Models/Category.cs
@@ -10,6 +10,7 @@ namespace LearnAspNetCoreMVC.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Required]
+        [Column("ProductName")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
         [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100 only!!")]
